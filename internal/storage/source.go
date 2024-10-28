3c46f9a8e13748aabe152ccf2fd838630c7644f6
Oleh <149747215+Primis1@users.noreply.github.com>
2024-10-28 15:23:11 -0400

    small fix

--- internal/storage/source.go
+++ internal/storage/source.go
@@ -4,7 +4,13 @@ import (
 	"context"
 	"github.com/jmoiron/sqlx"
 	model "news-bot/internal/model"
+	"news-bot/pkg/logging"
 )
+
+func idk() {
+	var log := logging.
+
+}
 
 type SourcePostgresStorage struct {
 	db *sqlx.DB
@@ -18,8 +24,12 @@ func (s *SourcePostgresStorage) Source(ctx context.Context) ([]model.Source, err
 
 	defer conn.Close()
 
+	var source []model.Source{
+
+	}
 }
-func (s *SourcePostgresStorage) SourceByID(ctx context.Context, id int64) (*model.Source, error) {}
+func (s *SourcePostgresStorage) SourceByID(ctx context.Context, id int64) (*model.Source, error) {
+}
 func (s *SourcePostgresStorage) Add(ctx context.Context, source model.Source) (int64, error)     {}
 func (s *SourcePostgresStorage) Delete(ctx context.Context, id int64) error                      {}
 
@@ -27,6 +37,5 @@ type dbSource struct {
 	ID        int64  `db:"id"`
 	Name      string `db:"name"`
 	FeedUrl   string `db:"feed_url"`
-	AddedAt   string `db:"added_at"`
 	CreatedAt string `db:"created_at"`
 }
